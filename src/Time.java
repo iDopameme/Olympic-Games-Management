6319d93fefff6f3e81582766a2648b92d562f02b
Gisellen <giselle.domingo.x@gmail.com>
2020-10-27 20:03:19 -0400

    Simplified tournamentDetails() by putting the time into Time.java

---
+++ src/Time.java
@@ -0,0 +1,88 @@
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Random;
+
+//// wip
+///// NEED TO FIX THE HOURS + make it cleaner coz it doesnt randomize all the hours between now and the end of the day
+
+public class Time {
+	//constants
+	private final int DEFAULT_SECONDS = 0;
+	private final int[] DEFAULT_MINUTES = {0, 30};
+	
+	//private members
+    private int year;
+    private int month;
+    private int day;
+    private int highDay;
+    private int lowDay;
+    private static int hourHigh;
+    private  static int hourLow;
+    private int hour;
+    private int minutes;
+    private int seconds;
+    private Calendar calendar;
+    private Random random;
+    private Date startTime;
+    private Date endTime;
+    
+    public Time() {
+    	init();
+    }
+    
+	public void init() {
+    	calendar = Calendar.getInstance();
+    	random = new Random();
+    	year = calendar.get(Calendar.YEAR);
+    	month = calendar.get(Calendar.MONTH);
+    	highDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); //get the maximum of days of current month
+    	lowDay = calendar.get(Calendar.DAY_OF_MONTH); //get todays date
+    	day = random.nextInt(highDay-lowDay) + lowDay; // this will print out a random date between today and the end of this month
+    	hourHigh = calendar.getActualMaximum(Calendar.HOUR); // max hours in a day
+    	hourLow = calendar.get(Calendar.HOUR); // get hour right now
+    	hour = random.nextInt(hourHigh - hourLow) + hourHigh;
+    	minutes = DEFAULT_MINUTES[(random.nextInt(DEFAULT_MINUTES.length))];
+    	seconds = DEFAULT_SECONDS;
+	}
+	
+	public void startTime() {
+    	//getting the random start time
+		if(hour > 12) { // if the time is more than 12AM/PM eg. 13:00
+			hour = hour - 12;
+		}
+    	calendar.set(year, month, day, hour, minutes, seconds);
+    	startTime = calendar.getTime();
+	}
+	
+	public void endTime() {
+		if(calendar.get(Calendar.HOUR_OF_DAY) + 2 > 12) { // if the time is more than 12AM/PM eg. 13:00
+			calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) + 2 - 12);
+		}
+		else {
+			calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) + 2);
+		} 
+    	minutes = DEFAULT_MINUTES[(random.nextInt(DEFAULT_MINUTES.length))]; //randomizing minutes again...
+    	calendar.set(Calendar.MINUTE, minutes);
+    	endTime = calendar.getTime();
+	}
+	
+	@Override
+	public String toString() {
+		return "DATE AND TIME\n"+
+				"START TIME: " + startTime +
+				"\nEND TIME: " + endTime;
+	}
+	
+	/////////
+	//TESTING
+	////////
+	
+//	public static void main(String[] args) {
+//		Time time = new Time();
+//		time.startTime();
+//		time.endTime();
+//		System.out.print(time);
+//	}
+//	
+}
--- src/Tournament.java
+++ src/Tournament.java
@@ -3,24 +3,10 @@ import java.util.Calendar;
 import java.util.Random;
 
 public class Tournament {
-	//constants
-	private final int DEFAULT_SECONDS = 0;
-	private final int[] DEFAULT_MINUTES = {0, 30};
-	
+
 	//private members
-    private int year;
-    private int month;
-    private int day;
-    private int highDay;
-    private int lowDay;
-    private int hour;
-    private int minutes;
-    private int seconds;
-    private Calendar calendar;
-    private Random random;
     private Participants players;
-    private Date startTime;
-    private Date endTime;
+    private Time time;
     private String firstPlace;
     private String secondPlace;
     private String thirdPlace;
@@ -31,40 +17,19 @@ public class Tournament {
 	}
 	
 	public void init() {
-		//setting objects
-    	calendar = Calendar.getInstance();
-    	random = new Random();
     	players = new Participants();
-
-    	//setting variables
-    	year = calendar.get(Calendar.YEAR);
-    	month = calendar.get(Calendar.MONTH);
-    	highDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); //get the maximum of days of current month
-    	lowDay = calendar.get(Calendar.DAY_OF_MONTH); //get todays date
-    	day = random.nextInt(highDay-lowDay) + lowDay; // this will print out a random date between today and the end of this month
-    	hour = 4; //i realized i did not randomize the hour :(
-    	minutes = DEFAULT_MINUTES[(random.nextInt(DEFAULT_MINUTES.length))];
-    	seconds = DEFAULT_SECONDS;
+    	time = new Time();
 	}
 	
     public void tournamentDetails(){
+    	//participants test
     	System.out.print("PLAYERS\n");
-    	players.listParticipants(); //participants test
+    	players.listParticipants();
     	
-    	System.out.print("\n\nDATE AND TIME\n");
-    	//getting the random start time
-    	calendar.set(year, month, day, hour, minutes, seconds);
-    	startTime = calendar.getTime();
-    	//getting the random end time
-    	calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) + 2); // setting the duration of game to 2 or 2 1/2 hrs for now to test
-    	minutes = DEFAULT_MINUTES[(random.nextInt(DEFAULT_MINUTES.length))]; //randomizing minutes again...
-    	calendar.set(Calendar.MINUTE, minutes);
-    	endTime = calendar.getTime();
-    	
-    	System.out.print("Start time: " + startTime);
-    	System.out.print("\nEnd time: " + endTime);
-    	
-    	
+    	//time test
+    	time.startTime(); 
+    	time.endTime();
+    	System.out.print(time);
     }
 
     public void playTournament(){
