bdb11e3c81be91376160788322ff33e596c9f83e
Richard W <Dopameme@outlook.com>
2020-10-30 17:23:21 -0400

    Added Connect.java to the project which maintains the connecting and disconnecting to the database.
    Made some debug changes to OlympicGames.java to test the functionality.
    
    Next is changing all the getters and setters and adding all the proper values to the database.

--- src/OlympicGames.java
+++ src/OlympicGames.java
@@ -1,16 +1,33 @@
-public class OlympicGames {
-    public static void main(String[] args) {
+import Database.Connect;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class OlympicGames {
+    public static void main(String[] args) throws SQLException {
+        // Class Instances
         Sports mySports = new Sports();
         Participants players = new Participants();
+        Connect database = new Connect();
 
-        mySports.setSports("Football");
-        mySports.setSports("Volleyball");
-        mySports.setSports("Tennis");
-        mySports.setSports("Athletics");
-        mySports.setSports("Swimming");
+        // Testing Sports.java
+//        System.out.println("----------------------TESTING SPORTS.JAVA---------------");
+//        mySports.setSports("Football");
+//        mySports.setSports("Volleyball");
+//        mySports.setSports("Tennis");
+//        mySports.setSports("Athletics");
+//        mySports.setSports("Swimming");
 
-        mySports.outputArrayList();
+        try {
+            database.startConn();
+            Statement stmt = database.getConn().createStatement();
 
+            String sql = "INSERT INTO countries VALUES" + "('101', 'Downtown Cafe', 'NY')"; // Testing the sql query statement (SUCCESS)
+            stmt.execute(sql);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
     }
 }
