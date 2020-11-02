3ac48515eb9510d678d8ff4b927b90d1422eb8b2
iDopameme <Dopameme@outlook.com>
2020-11-01 20:02:32 -0500

    Finished Countries.java & changed the SQL Dialect

--- src/OlympicGames.java
+++ src/OlympicGames.java
@@ -1,20 +1,13 @@
 import Database.Connect;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
-import java.io.File;
-
 public class OlympicGames {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         // Class Instances
-        Sports mySports = new Sports();
-        Participants players = new Participants();
-        Connect database = new Connect();
         Countries country = new Countries();
 
-        System.out.println(country.getCountryID("Belgium"));
+        //System.out.println(country.getCountryID("Belgium"));
+
+        country.removeAllValues();
 
     }
 }
