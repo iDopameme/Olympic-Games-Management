768caa1f08f0b885ded2ea8af6d0db68562aef9d
iDopameme <Dopameme@outlook.com>
2020-11-18 03:31:54 -0500

    Making overhaul changes with Tournament.java & olympic database tables.
    Not finished and still in progress.

--- src/Tournament.java
+++ src/Tournament.java
@@ -1,14 +1,17 @@
 import Database.Connect;
+import com.mysql.cj.protocol.Resultset;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.Random;
 
 public class Tournament {
 	//constants
@@ -27,6 +30,7 @@ public class Tournament {
 	Countries country;
 	Time time;
 	Timestamp timestamp;
+	Random random;
 	Participants players;
 	Team team;
 	List<Team> teams = new ArrayList<>();
@@ -45,59 +49,150 @@ public class Tournament {
 		time = new Time();
 		players = new Participants();
 		Scanner input = new Scanner(System.in);
+		random = new Random();
 
 		//menu start
 		System.out.println("****************************************************");
 		System.out.println("************* Creating Tournament Menu *************");
-
-		//setting sports
+		System.out.print("Tournament Name: ");
+		String tournament_name = input.nextLine();
 		sport.outputAllSports(conn);
-		System.out.println("+++ What sport will be played in the tournament?");
-		int countryChoice = input.nextInt();
-		userSport = sport.selectSport(countryChoice, conn);
-		type = sport.getSportType(countryChoice, conn);
-		System.out.println(userSport + " " + type);
-		System.out.println("How many teams will be in this tournament? (Max 32)");
-		int numOfTeams = input.nextInt();
-		this.numOfTeams = numOfTeams;
-		if (numOfTeams > MAX_TEAMS) {
-			System.out.println("Error! Team limit was exceeded. Tournament creation terminated...");
-			System.exit(1);
-		}
-		timestamp = time.setTime(); //setting time
+		System.out.println("Which sport will be played in this tournament? ");
+		String tournament_sport = input.nextLine();
+		Boolean validation = false;
+		do {
+			int random_id = random.nextInt(1000);
+			validation = createTournamentTable(conn, random_id, tournament_name, tournament_sport);
+		} while (!validation);
 
-
-		//setting participants based on sport type (WIP)
-		if (type.equals("Team")) {
-			country.participatingCountries(conn);
-			System.out.println("+++ What countries are participating? [Select by ID]\n(Select " + numOfTeams + " amount of teams)");
-			for (int i = 0; i < numOfTeams; i++) {
-				int playersInput = input.nextInt();
-				countries[i] = country.getCountries(playersInput, conn);
+		String sportType = sport.getSportType(sport.getSportID(tournament_name), conn);
+		if (sportType == "Team"){
+			System.out.print("How many countries will be in this tournament? ");
+			int teamCount = input.nextInt();
+			country.outputTable(conn);
+			System.out.print("Select " + teamCount + " countries by ID");
+			int[] teams = new int[teamCount];
+			for (int i = 0; i < teamCount; i++) {
+				teams[i] = input.nextInt();
+				int random_id = random.nextInt(1000);
+				createTeamTable(conn, random_id, country.getCountries(teams[i], conn), returnTournamentID(conn, tournament_name));
 			}
-			
-			//setting teams
-			for(int i = 0; i < numOfTeams; i++) {
-					team = new Team(countries[i]);
-					team.newTeam(countries[i], team);
-					teams.add(team);
-				}
-			//end creating tournament
-			addNewTournament(conn, numOfTeams, type, timestamp, userSport, countries);
-			System.out.println("+++ TOURNAMENT SUCCESSFULLY CREATED! +++");
-		} else if (type.equals("Individual")) {
-			players.listParticipants(type, conn);
-			System.out.println("+++ Who is participating?");
-			for (int i = 0; i < numOfTeams; i++)
-			{
-				int playersInput = input.nextInt();
-				playerNames[i] = players.getPlayerName(playersInput, conn);
+		} else if (sportType == "Individual"){
+			System.out.print("How many players will be in this tournament? ");
+			int playerCount = input.nextInt();
+			players.outputTable(conn);
+			System.out.print("Select " + playerCount + " players by ID");
+			int[] players = new int[playerCount];
+			for (int i = 0; i < playerCount; i++) {
+				players[i] = input.nextInt();
+				int random_id = random.nextInt(1000);
+				//createTeamTable(conn, random_id, players.ge)
 			}
-			addNewTournament(conn, numOfTeams, type, timestamp, userSport, playerNames);
-			System.out.println("+++ TOURNAMENT SUCCESSFULLY CREATED! +++");
 		}
-
 	}
+
+	public boolean createTournamentTable(Connect conn, int id, String t_name, String t_type) {
+		boolean validation;
+		try {
+			String query = "INSERT INTO olympics.Tournament" + "(id, tournament_name, tournament_type) VALUES" + "(?, ?, ?);"; // SQL Query
+			PreparedStatement pstmt = conn.getConn().prepareStatement(query); // Prepared Statement in order to pass values through SQL statements
+
+			pstmt.setInt(1, id);
+			pstmt.setString(2, t_name);
+			pstmt.setString(3, t_type);
+			pstmt.executeUpdate();
+			validation = true;
+
+		} catch (Exception ex) {
+			System.out.println("ERROR: " + ex.getMessage());
+			validation = false;
+		}
+		return validation;
+	}
+
+	public int returnTournamentID(Connect conn, String tournament_name) {
+		int tournament_ID = 0;
+		try {
+			String query = "SELECT id FROM olympics.Tournament WHERE tournament_name = ?";
+			PreparedStatement pstmt = conn.getConn().prepareStatement(query);
+			pstmt.setString(1, tournament_name);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				tournament_ID = rs.getInt("id");
+			}
+			rs.close();
+
+		} catch (SQLException ex) {
+			System.out.println("SQL exception occurred" + ex);
+		}
+		return tournament_ID;
+	}
+
+	public boolean createTeamTable(Connect conn, int id, String team_name, int tournament_id) {
+		boolean validation;
+		try {
+			String query = "INSERT INTO olympics.Team" + "(id, team_name, tournament_id) VALUES" + "(?, ?, ?);";
+			PreparedStatement pstmt = conn.getConn().prepareStatement(query);
+
+			pstmt.setInt(1, id);
+			pstmt.setString(2, team_name);
+			pstmt.setInt(3, tournament_id);
+			validation = true;
+
+		} catch (Exception ex) {
+			System.out.println("ERROR: " + ex.getMessage());
+			validation = false;
+		}
+		return validation;
+	}
+
+//	//setting sports
+//		sport.outputAllSports(conn);
+//		System.out.println("+++ What sport will be played in the tournament?");
+//	int countryChoice = input.nextInt();
+//	userSport = sport.selectSport(countryChoice, conn);
+//	type = sport.getSportType(countryChoice, conn);
+//		System.out.println(userSport + " " + type);
+//		System.out.println("How many teams will be in this tournament? (Max 32)");
+//	int numOfTeams = input.nextInt();
+//		this.numOfTeams = numOfTeams;
+//		if (numOfTeams > MAX_TEAMS) {
+//		System.out.println("Error! Team limit was exceeded. Tournament creation terminated...");
+//		System.exit(1);
+//	}
+//	timestamp = time.setTime(); //setting time
+//
+//
+//	//setting participants based on sport type (WIP)
+//		if (type.equals("Team")) {
+//		country.participatingCountries(conn);
+//		System.out.println("+++ What countries are participating? [Select by ID]\n(Select " + numOfTeams + " amount of teams)");
+//		for (int i = 0; i < numOfTeams; i++) {
+//			int playersInput = input.nextInt();
+//			countries[i] = country.getCountries(playersInput, conn);
+//		}
+//
+//		//setting teams
+//		for(int i = 0; i < numOfTeams; i++) {
+//			team = new Team(countries[i]);
+//			team.newTeam(countries[i], team);
+//			teams.add(team);
+//		}
+//		//end creating tournament
+//		addNewTournament(conn, numOfTeams, type, timestamp, userSport, countries);
+//		System.out.println("+++ TOURNAMENT SUCCESSFULLY CREATED! +++");
+//	} else if (type.equals("Individual")) {
+//		players.listParticipants(type, conn);
+//		System.out.println("+++ Who is participating?");
+//		for (int i = 0; i < numOfTeams; i++)
+//		{
+//			int playersInput = input.nextInt();
+//			playerNames[i] = players.getPlayerName(playersInput, conn);
+//		}
+//		addNewTournament(conn, numOfTeams, type, timestamp, userSport, playerNames);
+//		System.out.println("+++ TOURNAMENT SUCCESSFULLY CREATED! +++");
+//	}
 
 	public void modifyTournament(Connect conn) {
 		Scanner input = new Scanner(System.in);
@@ -215,52 +310,7 @@ public class Tournament {
     	}
 
 
-    public boolean addNewTournament(Connect conn, int numOfteams, String sportType, Timestamp gameTime, String sportName, String[] listOfTeams) {
-		boolean validation = false;
-		int gameID = 0;
-		try {
-			String query = "INSERT INTO olympics.games" + "(type, time, sport, numOfTeams) VALUES" + "(?, ?, ?, ?);"; // SQL Query
-			PreparedStatement pstmt = conn.getConn().prepareStatement(query); // Prepared Statement in order to pass values through SQL statements
 
-			pstmt.setString(1, sportType);
-			pstmt.setTimestamp(2, gameTime);
-			pstmt.setString(3, sportName);
-			pstmt.setInt(4, numOfteams);
-			pstmt.executeUpdate();
-
-			System.out.println("All values successfully inserted into the table.");
-			validation = true;
-
-			String sql = "SELECT tournamentID FROM olympics.games WHERE time = ?";
-			pstmt = conn.getConn().prepareStatement(sql);
-			pstmt.setTimestamp(1, gameTime);
-			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				gameID = rs.getInt("tournamentID");
-			}
-			rs.close();
-		}
-		catch (Exception ex) {
-			System.out.println("ERROR: " + ex.getMessage());
-		}
-
-		try {
-			String query = "INSERT INTO olympics.countryInGames" + "(foreignTourID, cName) VALUES" + "(?, ?);"; // SQL Query
-			PreparedStatement pstmt = conn.getConn().prepareStatement(query); // Prepared Statement in order to pass values through SQL statements
-
-			for (int i = 0; i < listOfTeams.length; i++) {
-				pstmt.setInt(1, gameID);
-				pstmt.setString(2, listOfTeams[i]);
-			}
-		}
-		catch (Exception ex) {
-			System.out.println("ERROR: " + ex.getMessage());
-			validation = false;
-		}
-
-		return validation;
-	}
 
 	public String getUserSport() {
 		return userSport.toUpperCase();
